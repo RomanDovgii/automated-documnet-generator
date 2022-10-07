7c151c315c078c56c308bcd15c59a51f5054db88
Roman Dovgii <romandovgiiwork@gmail.com>
2022-10-08 02:16:57 +0300

    Adds header to select

--- src/pages/select.js
+++ src/pages/select.js
@@ -1,10 +1,32 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 import Table from "../components/table";
+import { AppRoute } from "../utils/const";
 
 function Select() {
     return (
-        <div>
-            <Table/>
-        </div>
+        <Fragment>
+            <header>
+                <div>
+                    <Link to={AppRoute.MAIN}>Назад</Link>
+                    <form>
+                        <label>
+                            <input type="text"/>
+                        </label>
+                        <button>
+                            <span>Искать</span>
+                        </button>
+                    </form>
+                    <Link to={AppRoute.FORM}>Вперед</Link>
+                </div>
+            </header>
+            <main>
+                <Table/>
+            </main>
+            <footer>
+
+            </footer>
+        </Fragment>
     );
 }
 
